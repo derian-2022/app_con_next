df33c0c6176c0e2b94e1d992f51533cadcf64e45
derian-2022 <martinarias.edu@gmail.com>
2023-06-05 15:41:23 -0500

    app con next

---
+++ profile.js
@@ -0,0 +1,184 @@
+export const skills = [
+  {
+    skills: "javascript",
+    porcentage: 100,
+  },
+
+  {
+    skills: "CSS",
+    porcentage: 100,
+  },
+
+  {
+    skills: "React",
+    porcentage: 90,
+  },
+
+  {
+    skills: "Node.js",
+    porcentage: 75,
+  },
+
+  {
+    skills: "Bootstrap",
+    porcentage: 100,
+  },
+
+  {
+    skills: "React-Native",
+    porcentage: 40,
+  },
+
+  {
+    skills: "Tailwind CSS",
+    porcentage: 100,
+  },
+
+  {
+    skills: "Next.js",
+    porcentage: 30,
+  },
+];
+
+export const Experiences = [
+  {
+    title: "Habilidades con Node.js-Express",
+    description:
+      "Soy competente en el desarrollo de aplicaciones web con tecnologías backend como Node.js y Express. También tengo experiencia en el diseño y desarrollo de bases de datos utilizando MySQL y MongoDB.",
+    from: "Marzo 2023",
+    to: 2023,
+  },
+
+  {
+    title: "Habilidades con React-Hooks",
+    description:
+      "Mis principales competencias incluyen el dominio de lenguajes de programación y tecnologías como HTML, CSS y JavaScript. Estoy familiarizado con frameworks y bibliotecas populares como React, Node.js, entre otros. Además, tengo experiencia en el uso de herramientas como Bootstrap para crear interfaces modernas y escalables.",
+    from: "Enero 2023",
+    to: 2023,
+  },
+
+  {
+    title: "Habilidades con JavaScript",
+    description:
+      "Siempre me esfuerzo por mejorar la velocidad y el rendimiento de los sitios web que desarrollo. Me enfoco en optimizar el rendimiento utilizando técnicas como la compresión de archivos, el almacenamiento en caché y la minimización del tiempo de carga.",
+    from: "Noviembre 2022",
+    to: 2023,
+  },
+
+  {
+    title: "Habilidades con React-Native",
+    description:
+      "Como desarrollador web altamente motivado, estoy comprometido en ofrecer soluciones atractivas y funcionales que brinden una experiencia óptima a los usuarios. Me capacito dia a dia en diversas tecnologías y me esfuerzo por mejorar constantemente el rendimiento de los sitios web, Apps en Android/IOS.",
+    from: "Mayo 2023",
+    to: 2023,
+  },
+
+  {
+    title: "Habilidades con Next.js",
+    description:
+      "Como desarrollador web altamente motivado, estoy comprometido en ofrecer soluciones atractivas y funcionales que brinden una experiencia óptima a los usuarios. La práctica es fundamental para consolidar tus habilidades. Aplico lo que aprendo en proyectos personales o desafíos de programación... Obteniendo experiencia con la tecnologia Next.js, Apps en Android/IOS",
+    from: "Mayo 2023",
+  },
+];
+
+export const projects = [
+  {
+    name: "Awebsome website 🤗 1",
+    description:
+      "Quotes App: Componentes, useState, librerías: React JSX Componentes funcionales clase props introducción, hooks useState",
+    Image: "portfolio1.jpeg",
+  },
+
+  {
+    name: "Awebsome website 🤗 2",
+    description:
+      "Weather App: useEffect, custom hooks Ciclo de vida de un componente. useEffect. Comparación con los métodos de ciclo de vida: componentWillMount, componentWillUpdate, componentShouldReceiptProps... Axios, consumo de apis. custom hooks. Creación de useFetch para traer datos de una api",
+    Image: "portfolio2.jpeg",
+  },
+
+  {
+    name: "Awebsome website 🤗 3",
+    description:
+      "Rick and Morty: Temas varios Renderizado condicional. Listas y keys con map. Tipos de inputs Eventos e inputs controlados Vistazo a useCallback y useMemo",
+    Image: "portfolio3.png",
+  },
+
+  {
+    name: "Awebsome website 🤗 4",
+    description:
+      "CRUD de Usuarios: Formularios y CRUD' React Hook Form, comparación vs inputs controlados, useReducer. Ejemplos con ToDo lists. tokens y headers con axios",
+    Image: "portfolio4.png",
+  },
+
+  {
+    name: "Awebsome website 🤗 5",
+    description:
+      "Pokedex App: React Router e introducción a Redux. React Router Dom. Redireccionamiento. Parámetros en la URL. Rutas protegidas, introducción a Redux (store, actions, reducer, useDispatch, useSelector)",
+    Image: "portfolio5.jpeg",
+  },
+
+  {
+    name: "Awebsome website 🤗 6",
+    description:
+      "Ecommerce: Redux a profundidad y proyecto final. Middlewares. Autenticación con axios. Vistazo rápido a useContext y comparación con Redux",
+    Image: "portfolio6.jpeg",
+  },
+];
+
+export const posts = [
+  {
+    title: "React-Hooks",
+    content:
+      "Los hooks son unas funciones en react que nos permitirá “engancharnos” al estado y al ciclo de vida de la aplicación, entre otras funcionalidades. Se crearon específicamente para componentes funcionales. La forma de manejar estado en componentes clase es muy diferente. El estado, en palabras muy sencillas, son variables que van a almacenar y cambiar información de un componente.",
+    imageURL:
+      "https://res.cloudinary.com/practicaldev/image/fetch/s--Fn5V-TjG--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/ekjlwtc87ktcrzmahn9j.png",
+  },
+
+  {
+    title: "Node.js",
+    content:
+      "Mientras que el frontend es la capa de programación ejecutada en el navegador del usuario, el backend procesa la información que alimentará el frontend de datos. Es la capa de acceso a los datos, ya sea de un software o de un dispositivo en general, es la lógica tecnológica que hace que una página web funcione, lo que queda oculto a ojos del visitante. El backend de una solución",
+    imageURL:
+      "https://bs-uploads.toptal.io/blackfish-uploads/components/seo/content/og_image_file/og_image/1275225/secure-rest-api-in-nodejs-18f43b3033c239da5d2525cfd9fdc98f.png",
+  },
+
+  {
+    title: "JavaScript",
+    content:
+      "JavaScript moderno es un lenguaje de programación “seguro”. No proporciona acceso de bajo nivel a la memoria ni a la CPU (UCP); ya que se creó inicialmente para los navegadores, los cuales no lo requieren. Las capacidades de JavaScript dependen en gran medida en el entorno en que se ejecuta. Por ejemplo, Node.JS soporta funciones que permiten, JavaScript leer, escribir archivos.",
+    imageURL:
+      "https://itchief.ru/assets/images/covers/javascript-statements.png",
+  },
+
+  {
+    title: "Next.js",
+    content:
+      "Next.js es un framework de React que se enfoca en la facilidad de uso y la escalabilidad. Ofrece varias características para mejorar la velocidad de carga de la página y la experiencia del usuario, como el renderizado del lado del servidor (SSR) y el pre-renderizado estático. También tiene enrutamiento automático y una fácil configuración.",
+    imageURL:
+      "https://mobisoftinfotech.com/resources/wp-content/uploads/2022/04/next-JS-framework.png",
+  },
+
+  {
+    title: "React-Native",
+    content:
+      "React Native es un framework de programación de aplicaciones nativas multiplataforma que está basado en JavaScript y ReactJS. En React existe un “VirtualDOM”, en el que tenemos nuestro JSX, en el cual definimos los documentos HTML, y estos se transforman en componentes del navegador a través de JavaScript. Con React Native ocurre algo parecido, ya tenemos nuestros componentes JSX.",
+    imageURL:
+      "https://codersera.com/blog/wp-content/uploads/2019/02/react-native.png",
+  },
+
+  {
+    title: "CSS",
+    content:
+      "El CSS es un lenguaje basado en reglas: cada usuario define las reglas que especifican los grupos de estilos que van a utilizar a elementos particulares o grupos de elementos de la página web. El lenguaje CSS tiene la ventaja de ser mucho más simple, por lo que implica menos código y probabilidad de errores, así como una mayor velocidad de carga y facilidad de lectura. Además, con él tienes una mayor gama de posibilidades de edición para hacer que tu sitio luzca tal como quieres.",
+    imageURL:
+      "https://miro.medium.com/v2/resize:fit:600/1*OFsc0SD55jhi8cjo7aCA4w.jpeg",
+  },
+
+  {
+    title: "HTML",
+    content:
+      "HTML es un lenguaje de marcación de elementos para la creación de documentos hipertexto, muy fácil de aprender, lo que permite que cualquier persona, aunque no haya programado en la vida, pueda enfrentarse a la tarea de crear una web. HTML  así como a la incorporación de otros lenguajes  para definir el formato con el que se tienen que presentar las webs, CSS. <B>Esto está en negrita</B>.",
+    imageURL:
+      "https://img.freepik.com/foto-gratis/sistema-html-concepto-sitios-web_23-2150323542.jpg",
+  },
+];
